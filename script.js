97a307a69f5b46b1a118563009966c5b3927a8a5
Lewis Cooke <co0ke@hotmail.com>
2021-10-22 20:41:03 +0100

    Square dance

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿let canvas = document.querySelector('canvas')
+let context = canvas.getContext('2d');
+
+context.fillStyle = 'blue';
+
+for(let i = 0; i <= 1000; i++) {
+    context.lineWidth = 0.2;
+    context.beginPath();
+    context.rect(i * Math.random(), i * Math.random(), i * Math.random() * 80/i, i * Math.random() * 80/i);
+    context.stroke();
+}
